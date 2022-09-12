0a0db770efe0c6fae2862cca23b3ee86f0a15546
guido offermans <guidooffermans@hotmail.com>
2022-09-12 15:24:47 +0200

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+{
+	astroAllowShorthand: false;
+	printWidth: 120;
+	tabWidth: 2;
+	useTabs: true;
+	semi: true;
+	singleQuote: false;
+	quoteProps: "as-needed";
+	trailingComma: "es5";
+	bracketSpacing: true;
+}
